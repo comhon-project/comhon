9326bcfa0b12d27f6828d1e701e5589355e7bfdb
Jean-Philippe <jean-philippe@jeanphilippe-EASYNOTE-SL65.(none)>
2016-10-22 20:49:11 +0200

    fix on testXml manifest

--- tests/XmlTest.php
+++ tests/XmlTest.php
@@ -9,5 +9,5 @@ use objectManagerLib\object\singleton\InstanceModel;
 $lTestXmlModel = InstanceModel::getInstance()->getInstanceModel('testXml');
 $lTestXml = $lTestXmlModel->loadObject('plop2');
 
-$lTestXml->setValue('name', 'plop3');
+$lTestXml->setValue('name', 'plop4');
 $lTestXml->save();
